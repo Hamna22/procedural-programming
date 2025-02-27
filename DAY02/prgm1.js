7f6d1f13ddc442f5edef8275db96b2e04a2c4c76
Hamna22 <hamnana09@gmail.com>
2025-02-27 14:27:17 +0530

    try-catch error

---
+++ DAY02/prgm1.js
@@ -0,0 +1,22 @@
+console.log("start execution");
+try{
+exp='10/2***5'
+result = eval(exp)  //eval=>a predefined method for evaluation
+console.log(result);
+
+}
+catch(error){
+    console.log("expression is wrong");
+    
+
+}
+
+num1=10
+num2=20
+sum=num1+num2
+console.log(sum);
+
+console.log("end execution");
+
+
+
